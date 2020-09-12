70e96b828d70fe0853ee03fcde71cba17d9b09c2
Nazhim <nazhimkalam@gmail.com>
2020-09-12 21:39:41 +0530

    updated

--- src/aws/UPDATE-code.js
+++ src/aws/UPDATE-code.js
@@ -3,7 +3,7 @@
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
 
-exports.handler = function (event, context, callback) {
+exports.handler  = (event, context, callback) => {
 	const Id = event.Id;
 	const Sname = event.Sname;
 	const age = event.age;
