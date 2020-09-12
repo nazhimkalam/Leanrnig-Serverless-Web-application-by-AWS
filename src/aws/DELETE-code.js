55228258d0860b7db0a241a5e63201501092d84c
Nazhim <nazhimkalam@gmail.com>
2020-09-12 21:50:19 +0530

    Deleting works properly

--- src/aws/DELETE-code.js
+++ src/aws/DELETE-code.js
@@ -1,4 +1,24 @@
-// DELETE all 
+// DELETE all
 
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
+
+exports.handler = (event, context, callback) => {
+	const Id = event.Id;
+	const tableName = 'StudentDetails';
+
+	const params = {
+		TableName: tableName,
+		Key: {
+			Id: Id,
+		},
+	};
+
+	docClient.delete(params, (err, data) => {
+		if (err) {
+			callback(err, null);
+		} else {
+			callback(null, 'Successfully deleted the data');
+		}
+	});
+};
